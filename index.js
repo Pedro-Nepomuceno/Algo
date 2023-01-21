266a39ed657d4ebee020aa9c2188cb17c9b3efa8
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-01-20 16:50:10 -0800

    function explained

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//write a function called sumZero which accepts a sorted array of integers
+// The function should find the 1st pair where the sum is 0.
+//return an array that includes both values that sum to zero.
+
 function sumZero(arr) {
 	let left = 0;
 	let right = arr.length - 1;
