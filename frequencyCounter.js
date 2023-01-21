77c7a55ca009cb7e49a461d394aa7b683d597302
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-01-20 16:54:12 -0800

    frequencyCounter.js

---
+++ frequencyCounter.js
@@ -0,0 +1,3 @@
+// write a function called same, which accepts 2 arrays.
+// the function should return true if every value in the array has its corresponding value squared in the 2nd array
+// the frequency of values must be the same.
