e2510ffb192851d2b660a94beab54e6aa41e5712
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-05-06 15:01:22 -0700

    find 3 consec match

---
+++ SearchString.js
@@ -0,0 +1,17 @@
+// - Search Naive string
+// Dedine a function that acceptes two strigs, and search for following matching carachters
+
+function findMatchinglong(long, short) {
+	let counter = 0;
+
+	for (let i = 0; i <= long.length; i++) {
+		for (let j = 0; j <= short.length; j++) {
+			if (short[j] !== long[i + j]) {
+				break;
+			}
+			if (j === short.length - 1) {
+				counter++;
+			}
+		}
+	}
+}
