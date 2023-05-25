ba2914335d0a0ab04b0d1dcb537c2a564435d1ca
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-05-25 13:05:35 -0700

    partially solved

---
+++ TestProblem.js
@@ -0,0 +1,69 @@
+//Implement the following three functions of a basic JavaScript testing framework:
+//1. describe
+//This function defines a test suite of related testcases. Ittakes in a string testsuiteName and a callback func , which makes one or more calls
+//to "it).
+//2.'
+//This function defines a single test case in a test suite and is called within a descr-ibe •s callback Ifun€. It takes in a string testCaseNane and
+//its own callback func , which makes one or more calls to expect
+//3. expect
+//This function defines a single check in a testcase and is called within an e it •s callback func . It takes in an arbitrary parameter actual and
+//returns an object with the following three functions that are used to compare actual toother values:
+//1. . toExist())
+//This function checks that •ctuat is neither nult nor undefined .
+//2. :expect (actual) . toBe (expected
+//This function checks that actuat is stricttyequal to expected.
+//3. "expect (actual) . toBeType(type)
+
+function describe(testSuiteName, func) {
+	console.log(`beginning test suite ${testSuiteName}`);
+	try {
+		func();
+		console.log(`successfuly completed test suite ${testSuiteName}`);
+	} catch (error) {
+		const { testCaseName, errorMessage } = error;
+		console.error(
+			`failed running test suite ${testSuiteName} on` +
+				`test case ${testCaseName} with error message ${errorMessage}`
+		);
+	}
+}
+
+function it(testCaseName, func) {
+	console.log(`beggining test case ${testCaseName}`);
+	try {
+		func();
+		console.log(`successfully completed test case ${testCaseName}`);
+	} catch (errorMessage) {
+		throw { testCaseName, errorMessage };
+	}
+}
+
+function expect(actual) {
+	return new ExpectFunctions(actual);
+}
+
+class ExpectFunctions {
+	constructor(actual) {
+		this.actual = actual;
+		this.stringfiedActual = JSON.stringfy(actual);
+	}
+	toExist() {
+		if (this.actual == null) {
+			throw `expected value to exist but got ${this.stringfiedActual}`;
+		}
+	}
+	toBe(expected) {
+		if (this.actual !== expected) {
+			throw `expected ${this.stringfiedActual} to be ${JSON.stringy(
+				expected
+			)})`;
+		}
+	}
+	toBeType(type) {
+		if (typeof this.actual !== type) {
+			throw `expected ${
+				this.stringfiedActual
+			} to be type ${type} but got ${typeof this.actual}`;
+		}
+	}
+}
