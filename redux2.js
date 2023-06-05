68a5702513054c8127518a6e32bceba128200955
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-05 16:49:08 -0700

    fix func

--- redux2.js
+++ redux2.js
@@ -1,5 +1,12 @@
 import { useReducer } from "React";
 
+function reducer(state, { type, responseJSON, error }) {
+	switch (type) {
+		case "loading":
+			return { ...state, isLoading: true };
+	}
+}
+
 function useFetch() {
 	const [state, dispatch] = useReducer(reducer, {
 		responseJSON: null,
