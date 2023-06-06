e91cbcf42bed0c2c7690b388704dcd925a392b93
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-05 17:25:39 -0700

    fix func

--- redux2.js
+++ redux2.js
@@ -4,6 +4,12 @@ function reducer(state, { type, responseJSON, error }) {
 	switch (type) {
 		case "loading":
 			return { ...state, isLoading: true };
+		case "success":
+			return { responseJSON, isLoading: false, error: null };
+		case "error":
+			return { responseJSON: null, isLoading: false, error };
+		default:
+			throw new Error("unknow type");
 	}
 }
 
