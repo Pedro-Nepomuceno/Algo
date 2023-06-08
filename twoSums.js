25b8fa21f25b78ab1b343336cb2bcc575966458f
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-07 19:20:53 -0700

    twosums

---
+++ twoSums.js
@@ -0,0 +1,13 @@
+//write a function that takes an unsorted array and a target number, the function should return an array that sums to the target number
+
+function twoNumberSum(array, targetSum) {
+	let emptyArr = [];
+	for (let i = 0; i <= array.length; i++) {
+		for (let j = i + 1; j <= array.length - 1; j++) {
+			if (array[i] + array[j] === targetSum) {
+				emptyArr.push(array[i], array[j]);
+			}
+			return emptyArr;
+		}
+	}
+}
