7977576c6a20cc2c0a85a01a16b0ad26ccd5fae9
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-07 20:51:41 -0700

    opti sol

--- twoSums.js
+++ twoSums.js
@@ -1,5 +1,6 @@
 //write a function that takes an unsorted array and a target number, the function should return an array that sums to the target number
 
+// not optimum solution
 function twoNumberSum(array, targetSum) {
 	let emptyArr = [];
 	for (let i = 0; i <= array.length; i++) {
@@ -10,4 +11,17 @@ function twoNumberSum(array, targetSum) {
 			return emptyArr;
 		}
 	}
+}
+
+// optimum solution
+
+function twoNumberSum(array, targetSum) {
+	let emptyArr = {};
+	for (let val of array) {
+		let potentialSum = targetSum - val;
+		if (potentialSum in emptyArr) {
+			return [potentialSum, val];
+		} else emptyArr[val] = true;
+	}
+	return [];
 }
