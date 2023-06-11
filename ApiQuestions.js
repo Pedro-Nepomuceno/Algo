503fe19216183c742e6f18a687b6e0180b41f9f7
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-11 14:06:17 -0700

    fix typ

--- ApiQuestions.js
+++ ApiQuestions.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const QUESTIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/questions';
-const SUBMISSIONS_API_BASE_URL = 'https://api.frontendexpert.io/api/fe/submissions';
+const QUESTIONS_API_BASE_URL = "https://api.frontendexpert.io/api/fe/questions";
+const SUBMISSIONS_API_BASE_URL =
+	"https://api.frontendexpert.io/api/fe/submissions";
 
 export default function QuestionList() {
-  const [questions, setQuestions] = useState();
-  const[submission, setSubmission] = useState();
+	const [questions, setQuestions] = useState();
+	const [submission, setSubmission] = useState();
 
-useEffect(()={
-  const fetchQuestion = fetch(QUESTIONS_API_BASE_URL);
-  const questionConverter = fetchQuestion.json();
+	useEffect(() => {
+		const fetchQuestion = fetch(QUESTIONS_API_BASE_URL);
+		const questionConverter = fetchQuestion.json();
+	}, []);
 
-},[])
+	useEffect(() => {}, []);
 
-useEffect(()={},[])
-
-  return (
-    <>
-    </>
-  );
+	return <></>;
 }
