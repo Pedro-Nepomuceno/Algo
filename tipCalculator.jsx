e450c6eaeabc45332d7d056b9e4e67c5c025130d
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-10 17:58:53 -0700

    advance func

--- tipCalculator.jsx
+++ tipCalculator.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
+import React from "react";
 
 export default function TipCalculator() {
-const [bill,setBill] = useState('50');
-const [tip,setTip] = useState('9');
-const[people,setNumberPeople] = useState('1');
+	const [bill, setBill] = useState(50);
+	const [tip, setTip] = useState(9);
+	const [people, setNumberPeople] = useState(1);
 
-  const handleChange = (e)=>{
-    setBill(parseInt(e.target.value));
-  }
-const handleChangeTip = (e)=>{
-  setTip(e.target.value)
-}
-const handleChangePeople = (e)=>{
-  setNumberPeople(e.target.value)
-}
-  function handleTotalTip() {
-    return (
-      {bill%tip}
-    )
-  }
-  
-  return (
-    <>
-      <label htmlFor="bill">Bill</label>
-      <input
-        id="bill"
-        type='number'
-        value={bill}
-        onChange={handleChange}>{bill}</input>
-      <input
-        value={tip}
-        onChange={handleChangeTip}>{bill}</input>
-      <input
-        value={bill}
-        onChange={handleChangePeople}>{bill}</input>
-      <p>Total tip {}</p>
-      <p>Tip Per Person: {}</p>
-    </>
-  );
+	const handleChange = (e) => {
+		setBill(parseInt(e.target.value));
+	};
+	const handleChangeTip = (e) => {
+		setTip(parseInt(e.target.value));
+	};
+	const handleChangePeople = (e) => {
+		setNumberPeople(e.target.value);
+	};
+
+	return (
+		<>
+			<label htmlFor="bill">Bill</label>
+			<input
+				id="bill"
+				type="number"
+				min="0"
+				value={bill}
+				onChange={handleChange}
+			/>
+			<label htmlFor="tipPercentage">Tip Percentage</label>
+			<input
+				id="tipPercentage"
+				value={tip}
+				type="number"
+				onChange={handleChangeTip}
+			/>
+			<label htmlFor="people">Number of People</label>
+			<input
+				id="people"
+				value={bill}
+				type="number"
+				onChange={handleChangePeople}
+			/>
+			<p>Total tip {}</p>
+			<p>Tip Per Person: {}</p>
+		</>
+	);
 }
