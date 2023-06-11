baabfa0cbfd948df5aea15ae73ea178467c7b442
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-10 17:49:37 -0700

    prog

--- tipCalculator.jsx
+++ tipCalculator.jsx
@@ -1,31 +1,41 @@
-import React from "react";
+import React from 'react';
 
 export default function TipCalculator() {
-	const [bill, setBill] = useState("50");
-	const [tip, setTip] = useState("9");
-	const [people, setNumberPeople] = useState("1");
+const [bill,setBill] = useState('50');
+const [tip,setTip] = useState('9');
+const[people,setNumberPeople] = useState('1');
 
-	const handleChange = (e) => {
-		setBill(e.target.value);
-	};
-	const handleChangeTip = (e) => {
-		setTip(e.target.value);
-	};
-	const handleChangePeople = (e) => {
-		setNumberPeople(e.target.value);
-	};
-
-	return (
-		<>
-			<input value={bill} onChange={handleChange}>
-				{bill}
-			</input>
-			<input value={tip} onChange={handleChangeTip}>
-				{bill}
-			</input>
-			<input value={bill} onChange={handleChangePeople}>
-				{bill}
-			</input>
-		</>
-	);
+  const handleChange = (e)=>{
+    setBill(parseInt(e.target.value));
+  }
+const handleChangeTip = (e)=>{
+  setTip(e.target.value)
+}
+const handleChangePeople = (e)=>{
+  setNumberPeople(e.target.value)
+}
+  function handleTotalTip() {
+    return (
+      {bill%tip}
+    )
+  }
+  
+  return (
+    <>
+      <label htmlFor="bill">Bill</label>
+      <input
+        id="bill"
+        type='number'
+        value={bill}
+        onChange={handleChange}>{bill}</input>
+      <input
+        value={tip}
+        onChange={handleChangeTip}>{bill}</input>
+      <input
+        value={bill}
+        onChange={handleChangePeople}>{bill}</input>
+      <p>Total tip {}</p>
+      <p>Tip Per Person: {}</p>
+    </>
+  );
 }
