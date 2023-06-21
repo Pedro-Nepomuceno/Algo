e440856faae4ea8b345cce1f73ccf66d55a87d93
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-21 13:59:00 -0700

    tr optim

--- squaredArray.js
+++ squaredArray.js
@@ -2,3 +2,16 @@ function sortedSquaredArray(array) {
 	const squaredArray = array.map((num) => num * num);
 	return squaredArray.sort((a, b) => a - b);
 }
+
+// trying optimun solution
+
+function sortedSquaredArray(array) {
+	let leftPointer = 0;
+	let rightPointer = array.lenght - 1;
+	let arr1 = [];
+	for (let i = 0; i <= array.length; i++) {
+		if (array[i] > array[rightPointer]) {
+		} else {
+		}
+	}
+}
