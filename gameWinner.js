9aa99b9d62a51a39cb7c592b96c295d497de22bc
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-23 13:02:42 -0700

    set func

---
+++ gameWinner.js
@@ -0,0 +1,5 @@
+// given an array of pairs representing the team and the result of a game, where 0 represent that the away team won
+// and 1 represent the home team won. find the team that won the tournament
+function tournamentWinner(competitions, results) {
+	return "";
+}
