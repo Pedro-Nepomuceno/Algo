102ea34b49a125316bbd632695684393b520e19a
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-22 18:52:09 -0700

    opt sol

--- squaredArray.js
+++ squaredArray.js
@@ -9,9 +9,9 @@ function sortedSquaredArray(array) {
 	const newArray = new Array(array.length).fill(0);
 	let leftPointer = 0;
 	let rightPointer = array.lenght - 1;
-	for (let i = 0; i <= array.length; i++) {
-		if (array[i] > array[rightPointer]) {
-		} else {
-		}
+	let index = array.length - 1;
+	while (leftPointer >= rightPointer) {
+		let leftVal = Math.pow(array[leftPointer], 2);
+		let rightVal = Math.pow(array[rightPointer], 2);
 	}
 }
