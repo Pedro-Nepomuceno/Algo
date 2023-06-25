69f3dcd7be6da13ffb94faaa497b08da8ba14941
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-25 14:20:49 -0700

    sortBuiltIn

--- sortBuiltIn.js
+++ sortBuiltIn.js
@@ -3,3 +3,11 @@
 function sortAlgo(num1, num2) {
 	return num1 - num2;
 }
+
+let arrayToSort = [10, 39, 49, 4, 2, 59];
+
+arrayToSort.sort(sortAlgo);
+
+// expected output
+
+[2, 4, 10, 39, 49, 59];
