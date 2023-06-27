193fdf4d16c2305b8c94aaa52214c366b86058f6
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-26 17:11:46 -0700

    not opt sol

--- Bubblesort.js
+++ Bubblesort.js
@@ -9,3 +9,18 @@ function bubbleSort(array) {
 		}
 	}
 }
+
+// not optimum version
+
+function bubble(array) {
+	for (let i = 0; i <= array.length; i++) {
+		for (let j = 0; j <= array.length; j++) {
+			if (array[j] > array[j + 1]) {
+				let tempVal = array[j];
+				array[j] = array[j + 1];
+				tempVal = array[j];
+			}
+		}
+	}
+	return array;
+}
