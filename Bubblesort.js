4bda4f54ad2930d462f9a5ecec01c4ef6296a36a
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-26 21:01:07 -0700

    es15 solution

--- Bubblesort.js
+++ Bubblesort.js
@@ -38,3 +38,19 @@ function bubbleSort(array) {
 	}
 	return array;
 }
+
+//ES15 syntax solution
+
+function bubbleSort2(arr) {
+	const swap = (arr, Idx1, Idx2) => {
+		[arr[Idx1], arr[Idx2]] = [arr[Idx2], arr[Idx1]];
+	};
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; i++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+	return arr;
+}
