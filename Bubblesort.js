7c5558ffeaa920cf6a2eb68b8a8dc9eb1d22a397
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-27 12:06:09 -0700

    other way to solve

--- Bubblesort.js
+++ Bubblesort.js
@@ -56,3 +56,28 @@ function bubbleSort2(arr) {
 }
 
 //By using tempVal as an intermediary variable during the swapping process, we ensure that the elements are correctly swapped without losing any values. This helps maintain the integrity of the sorting algorithm.
+function bubbleSort2(arr) {
+	const swap = (arr, Idx1, Idx2) => {
+		[arr[Idx1], arr[Idx2]] = [arr[Idx2], arr[Idx1]];
+	};
+	for (let i = arr.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; i++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr, j, j + 1);
+			}
+		}
+	}
+	return arr;
+}
+function bubbleSort(array) {
+	for (let i = array.length; i > 0; i--) {
+		for (let j = 0; j < i - 1; i++) {
+			if (array[j] > array[j + 1]) {
+				let tempVal = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tempVal;
+			}
+		}
+	}
+	return array;
+}
