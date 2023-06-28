3ea11b2d8d0f59cc2ed7ce4d76645668ea0697f6
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-06-28 16:19:16 -0700

    find the unique value

---
+++ cleanObject.js
@@ -0,0 +1,17 @@
+// given an object, clean the values that repeat
+
+let object = {
+	value1: "1m",
+	value2: "1m",
+	value3: "2m",
+	value4: "2m",
+};
+
+function cleanData(obj) {
+	const valueCounts = {};
+	const uniqueObj = {};
+
+	for (const key in obj) {
+		const value = obj[key];
+	}
+}
