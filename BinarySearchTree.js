5127a1a80b8277bfb137e7c02b59bf4aac1d5e7e
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-07-06 20:53:24 -0700

    one sol

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -1,5 +1,18 @@
 // write a function that takes a tree and a target value. and find what is the closest value to the target value
 
 function findClosestValueBst(tree, target) {
+	let closestValue = null;
+	let minDiff = infinity;
+	let node = tree;
+
+	while (node) {
+		const diff = Math.abs(node.value - target);
+		if (diff <= minDiff) {
+			minDiff = diff;
+			closestValue = node.value;
+		}
+		node = target < node.value ? node.left : node.right;
+	}
+
 	return closestValue;
 }
