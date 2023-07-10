fd589d53b0364aa89277cee5e3f2219a4ee909ad
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-07-10 10:35:35 -0700

    doc func

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,13 +1,13 @@
-function insertionSort(arr) {
-	for (let i = 1; i <= arr.length; i++) {
-		let currentVal = arr[i];
-		for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-			arr[j + 1] = arr[j];
-		}
-		arr[j + 1] = currentVal;
-	}
-	return arr;
-}
+// function insertionSort(arr) {
+// 	for (let i = 1; i <= arr.length; i++) {
+// 		let currentVal = arr[i];
+// 		for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+// 			arr[j + 1] = arr[j];
+// 		}
+// 		arr[j + 1] = currentVal;
+// 	}
+// 	return arr;
+// }
 [6, 3, 1, 5];
 
 function insertionSort(arr) {
