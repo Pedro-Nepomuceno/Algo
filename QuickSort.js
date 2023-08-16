bffc844c9b37923176eaa7968d24a54707962c1b
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-08-16 12:28:39 -0700

    set helper

--- QuickSort.js
+++ QuickSort.js
@@ -1,7 +1,22 @@
 // implemnt quick sort algo
 
+// implement a function to swap
+
 
 // first implement a helper function to place element in the right index
 function helperFunction (arr, start =0, end = arr.length+1){
-    
+    function swap (array, i, j) {
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        
+        }
+    pivot = arr[start];
+    swapIndex = start;  
+    for(let i = start +1; i< arr.length ; i++){
+        if(pivot > i){
+            swapIndex++
+        }
+    }    
+
 }
