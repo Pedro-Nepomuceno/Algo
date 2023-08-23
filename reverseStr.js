99a39a685307f939276a612830254aa2d967d9e6
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-08-22 18:47:10 -0700

    another sol

--- reverseStr.js
+++ reverseStr.js
@@ -4,3 +4,14 @@ function reverseStr(str){
     return str.split('').reverse().join('');
 }
 
+
+// hello
+function rvrStr (str) {
+    let reverseStr = "";
+    for(let char of str){
+ reverseStr = char + reverseStr;
+ //             h + "";
+ //             e + h;
+    }
+    return reverseStr
+}
