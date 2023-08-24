896db1e6ab954ef8f257f8f9529a246a7c4a59c9
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-08-24 13:26:41 -0700

    set problem

--- findCommomChart.js
+++ findCommomChart.js
@@ -1,1 +1,11 @@
 // given two arrays, return true if they share a character
+
+// loop thru the array and create a object with keys that we already pass by; 
+
+function checkSimilar(arr1,arr2) {
+    map = {};
+
+
+
+    
+}
