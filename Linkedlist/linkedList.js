547e9b3a36e06cb1b6fbcdde56d4c004abcde962
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-09-13 11:34:12 -0700

    updatE

--- Linkedlist/linkedList.js
+++ Linkedlist/linkedList.js
@@ -1,1 +1,20 @@
 // create new linkedList 
+
+class Node {
+    constructor(value){
+        this.head = {
+        value:value,
+        next: null,
+        }
+        this.tail = this.head;
+        this.length = 1;
+    }
+   
+   
+
+newNode(){
+    const node = {
+    
+    }
+}
+}
