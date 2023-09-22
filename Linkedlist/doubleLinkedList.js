1331d163647d5cae5107a3512964acd0e2396238
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-09-22 15:19:50 -0700

    doubleLinked

--- Linkedlist/doubleLinkedList.js
+++ Linkedlist/doubleLinkedList.js
@@ -8,6 +8,32 @@ class DoublelinkedList{
             prev: null
         }
         this.tail = this.head;
-        this.next = null
+        this.length = 1;
     }
+
+    append(value){
+        const newNode = {
+            valeu: value,
+            next: null,
+            prev: null,
+        }
+        newNode.prev = this.tail
+        this.tail.next = newNode
+        this.tail = newNode;
+        this.length++
+        return this
+    }
+prepend(value){
+    const newNode = {
+        value:value,
+        next:null,
+        prev:null,
+    }
+    newNode.next = this.head;
+    this.head.prev = newNode
+    this.head = newNode;
+    this.length++
+    return this
+}
+
 }
