383d03183059b1f520667f5f4b9ab1e0f906f18c
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-09-22 12:25:31 -0700

    update

--- Linkedlist/doubleLinkedList.js
+++ Linkedlist/doubleLinkedList.js
@@ -1,1 +1,13 @@
 // set a double linked list
+
+class DoublelinkedList{
+    constructor(value){
+        this.head = {
+            value:value,
+            next: null,
+            prev: null
+        }
+        this.tail = this.head;
+        this.next = null
+    }
+}
