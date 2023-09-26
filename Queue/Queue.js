63b346c7752a167e862d35489b07c22ff69e0e16
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-09-26 12:31:34 -0700

    update

---
+++ Queue/Queue.js
@@ -0,0 +1,25 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+        this.length = 0;
+    }
+    
+}
+
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+    peek(){
+
+    }
+    enqueue(){
+
+    }
+    dequeue(){
+        
+    }
+}
