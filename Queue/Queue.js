9ddf31ef38279d7b6b94d76c477d037fc6937980
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-09-26 12:44:43 -0700

    update

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -14,12 +14,31 @@ class Queue{
         this.length = 0;
     }
     peek(){
-
+return this.first
     }
-    enqueue(){
-
+    enqueue(value){
+const newNode = new Node(value);
+if(this.length === 0){
+    this.first = newNode;
+    this.last = newNode;    
+}
+else{
+    this.last.next = newNode
+    this.last = newNode
+    this.length++
+}
+return this
     }
     dequeue(){
-        
+        if(!this.first){
+            return null
+        }
+        if(this.first === this.last){
+            this.last = null
+        }
+        this.first = this.first.next
+        this.length-- 
+        return this
     }
+    
 }
