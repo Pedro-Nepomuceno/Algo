0770f044e461c71f9306ce520a5493cf316bd288
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-09-27 14:15:48 -0700

    update

--- callStack/QueueAndStack.js
+++ callStack/QueueAndStack.js
@@ -1,2 +1,17 @@
 // implement queue using stacks 
 
+class QueueUsingStacks{
+    constructor(){
+        this.pushStack = [];
+        this.popStack = [];
+    }
+    peek(){
+
+    }
+    push(value){
+    this.pushStack.push(value)
+    }
+    pop(){
+if(!this.)
+    }
+}
