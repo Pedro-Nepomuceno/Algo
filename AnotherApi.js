b9dcea10b132a5cea5e2199ca8492c9274520ca2
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2023-10-02 12:47:09 -0700

    update

--- AnotherApi.js
+++ AnotherApi.js
@@ -1,54 +1,54 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-const MyComponent = () ={
-const [data, setData] = useState([]);
-const [isLoading, setIsloading] = useState(true);
+// const MyComponent = () ={
+// const [data, setData] = useState([]);
+// const [isLoading, setIsloading] = useState(true);
 
-useEffect(()=>{
-    const fetchData = async ()=>{
-    try{
-        const response = await fetch(someUrl);
-        const responseJson = await response.json();
-        setData(responseJson);
-        setIsloading(false);
-    }
-    catch(error){
+// // useEffect(() => {
+// //     const fetchData = async ()=>{
+// //     try{
+// //         const response = await fetch(someUrl);
+// //         const responseJson = await response.json();
+// //         setData(responseJson);
+// //         setIsloading(false);
+// //     }
+// //     catch(error){
 
-console.log(`error fetching data ${error}`)
-setIsloading(false);
-    }
-    }
+// // console.log(`error fetching data ${error}`)
+// // setIsloading(false);
+// //     }
+// //     }
 
 
 
-    return fetchData
-},[]);
+// //     return fetchData
+// // });
 
-return(
+// return(
 
-<div>
-<ul>
-{data.map((item) =>{
-    <li key={item.id}>{item.name}</li>
-}) }
-</ul>
-</div>
+// <div>
+// <ul>
+// {data.map((item) =>{
+//     <li key={item.id}>{item.name}</li>
+// }) }
+// </ul>
+// </div>
 
-)
+// )
 
-// [4,5,6,9] 9 
-function findPairSun(array,sum){
-    let map = {};
-    for(let num of array){
-        let potentialSum = sum - num;
-        if(potentialSum in map)
-        return [num, potentialSum]
-    }
-    else {
-        map[potentialSum] = true;
-    }
+// // [4,5,6,9] 9 
+// function findPairSun(array,sum){
+//     let map = {};
+//     for(let num of array){
+//         let potentialSum = sum - num;
+//         if(potentialSum in map)
+//         return [num, potentialSum]
+//     }
+//     else {
+//         map[potentialSum] = true;
+//     }
 
-}
+// }
 
 
 
