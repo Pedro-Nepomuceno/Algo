0657d217ebcac8d8b0c9e2b066f202cf595e2e63
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-03-07 14:35:45 -0800

    update unique value

--- Array/UniquePath.js
+++ Array/UniquePath.js
@@ -1,3 +1,7 @@
 // dynamic programming
 // A robot is located in the top left corner of a m x n grid
 // the robot can only move either down or right at any point in time, the robot is trying to reach the bottom right corner of the grid
+
+const uniquePath = function (m, n) {
+  const table = Array.from({ length: m }, () => new Array(n));
+};
