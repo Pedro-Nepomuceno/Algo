db9eb5b30cc741eb16370432510f8ff2e0968e8e
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-05-20 20:53:21 -0700

    ts algo

--- Array/BubbleSort.ts
+++ Array/BubbleSort.ts
@@ -2,3 +2,14 @@ interface User {
   name: string;
   id: number;
 }
+
+
+function bubbleSort (arr: number[]): number[] {
+
+    for (let i = 0; i<= arr.length; i++){
+        if(arr[i] >ki arr[i+1]){
+
+        }
+    }
+    return arr
+}
