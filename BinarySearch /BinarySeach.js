3bea18579347a21a1b1c0c2277c6bad6619a8079
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-06-03 18:40:05 -0700

    set problem

--- BinarySearch /BinarySeach.js
+++ BinarySearch /BinarySeach.js
@@ -1,1 +1,3 @@
-// assume you have an Array of commits [c1,c3,c4,c5]
+// assume you have an Array of commits [c1,c3,c4,c5] and you want to find out which commit was the bad one which causes all the following one to be bad
+// you are given an API boolean isBadVersion() which will return whether a commit is bad.
+// implement a function to find the first bad commit
