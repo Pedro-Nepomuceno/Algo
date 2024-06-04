e06238419317416ef7670d2455b409998d844fe2
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-06-03 18:37:47 -0700

    start algo and data

--- Array/BubbleSort.ts
+++ Array/BubbleSort.ts
@@ -3,13 +3,13 @@ interface User {
   id: number;
 }
 
-
-function bubbleSort (arr: number[]): number[] {
-
-    for (let i = 0; i<= arr.length; i++){
-        if(arr[i] >ki arr[i+1]){
-
-        }
+function bubble(arr: number[]): number[] {
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      let temporary = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temporary;
     }
-    return arr
+  }
+  return arr;
 }
---
+++ BinarySearch /BinarySeach.js
@@ -0,0 +1,1 @@
+// assume you have an Array of commits [c1,c3,c4,c5]
