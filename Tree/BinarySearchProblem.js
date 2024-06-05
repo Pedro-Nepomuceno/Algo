288bdc93358b4bf6f5deb59bbfe42e6f590c9b98
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-06-05 15:00:11 -0700

    solve height problem

--- Tree/BinarySearchProblem.js
+++ Tree/BinarySearchProblem.js
@@ -1,1 +1,14 @@
 // find the most frequent value
+function inOrderDFS(self, node) {
+  if (node == null) return;
+}
+
+// given an tree determine if the tree is balanced or not
+// a tree is balanced if the height of the two subtree of every node never differ more than 1
+
+function findTheHeight(root) {
+  if (root == null) return;
+  let left = findTheHeight(root.left);
+  let right = findTheHeight(root.right);
+  return max(left, right) + 1;
+}
