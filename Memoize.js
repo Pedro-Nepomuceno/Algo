90cb8f3d7350729e0fc88663292153c9ea4483d6
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-07-08 19:53:02 -0700

    set pure function

--- Memoize.js
+++ Memoize.js
@@ -1,1 +1,13 @@
 // this only work with pure function. A pure function is defined as a function that always returns the same output given the same input and doesnt have any side effect
+function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (key in cash) {
+      return cache[key];
+    }
+    cache[key] = fn(...args);
+    return cache[key];
+  };
+}
