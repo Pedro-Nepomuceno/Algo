5642f00f5c2a1dbd60cd8aa148acc06187660562
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-08-05 16:11:37 -0700

    two pointers

---
+++ TwoPointer.java
@@ -0,0 +1,9 @@
+public static boolean detectCycle(Node head) {
+    Node fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
