0cd12ef96623950d802ab7e243bf42de5ad848c7
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-08-05 15:36:50 -0700

    start algo

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+// given a string return true if it is a palindrome
+// palindrome are string that form the same word if it is reversed
+
+function palindrome(str) {
+  // first I need to iterate over the str and reverse it for comparisson
+  // second I need to compare original str with reversed string
+  // return true if str === reversedStr
+}
