7f4f4464a4112f639d4978bba1689a4fa78c6293
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-08-14 14:07:58 -0700

    another way to solve

--- MostCommumLetter.js
+++ MostCommumLetter.js
@@ -17,3 +17,23 @@ function FindMostCommum(str) {
   }
   return maxChar;
 }
+
+// another way to loop through object
+
+function FindMostCommum2(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    charMap[char] = (charMap[char] || 0) + 1;
+  }
+
+  for (let key in charMap) {
+    if (charMap[key] > max) {
+      max = charMap[key];
+      maxChar = key;
+    }
+  }
+  return maxChar;
+}
