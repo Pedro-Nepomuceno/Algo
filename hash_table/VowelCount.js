4a36e62d7e6fe280589492df121c03d0572081cc
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-08-26 15:05:16 -0700

    solution vowel count

--- hash_table/VowelCount.js
+++ hash_table/VowelCount.js
@@ -1,1 +1,17 @@
 // write a function that returns the numbers of vowels used in a string
+
+function countVowel(str) {
+  // iterate over the string
+  // check to see if iteration contains vowels
+  // keep count of variable
+  let newStr = str.lowerCase().replace(/[\W]/, "");
+  let count = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+  for (let char of newStr) {
+    if (vowels.includes(char)) {
+      count += 1;
+    }
+  }
+
+  return count;
+}
