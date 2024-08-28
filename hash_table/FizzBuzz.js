88889d54200b51218d2b39a7e2f73f0ec4b2bf23
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-08-27 18:20:04 -0700

    set problem

--- hash_table/FizzBuzz.js
+++ hash_table/FizzBuzz.js
@@ -7,11 +7,9 @@ function fizzbuzz(number) {
   while (number < 0) {
     if (number % 3 == 0 && number % 5 == 0) {
       console.log("fizzbuzz");
-    }
-    if (number % 3 == 0) {
+    } else if (number % 3 == 0) {
       console.log("fizz");
-    }
-    if (number % 5 == 0) {
+    } else if (number % 5 == 0) {
       console.log("BUZZ");
     } else {
       console.log(number);
