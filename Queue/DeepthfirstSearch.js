147a29b3f7e4cebe8253e1a0c7c6762684eea885
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-09-03 14:14:44 -0700

    set problem

---
+++ Queue/DeepthfirstSearch.js
@@ -0,0 +1,1 @@
+// represent a graph with the airporters and the possible routes each plane can make for given airport
--- hash_table/piramide.js
+++ hash_table/piramide.js
@@ -1,16 +1,21 @@
 // write a function that accepts a positive number N
-// the function should console.log a step shape
+// the function should console.log a pyramide shape
 // with N levels using the character #
-// make sure the steps has space on the right side
+// make sure the steps has space on the right side and left side
 
-function steps(n) {
+// "   #  "
+// "  ##  "
+// "#####"
+
+function piramide(n) {
+  const mid = Math.floor((2 * n - 1) / 2);
   for (let row = 1; row <= n; row++) {
     let line = "";
-    for (let col = 1; col <= n; col++) {
-      if (col <= row) {
+    for (let col = 1; col < 2 * n - 1; col++) {
+      if (col >= mid - row && col <= mid + row) {
         line += "#";
       } else {
-        line += "";
+        line += " ";
       }
     }
     console.log(line);
