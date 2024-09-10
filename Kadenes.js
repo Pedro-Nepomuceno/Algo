a9ac80cb157e05a87da278f2c94476f75fc62313
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-09-09 19:28:15 -0700

    n2 solution

--- Kadenes.js
+++ Kadenes.js
@@ -1,1 +1,17 @@
 // find a non empty subarray with the largest sum
+
+// example [4, -1, 2, -7, 3, 4]
+
+function findLargestSum(array) {
+  let largerstSum = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    let currentSum = 0;
+    for (let j = 1; j < array.length; j++) {
+      currentSum = +array[j];
+    }
+    if (currentSum > largerstSum) {
+      largerstSum = currentsum;
+    }
+  }
+}
