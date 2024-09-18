0c53340c31a0a06fc5fb29f1e635b0c59889faf0
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-09-17 20:00:39 -0700

    linear time solution

--- Array/concatenationArray.js
+++ Array/concatenationArray.js
@@ -2,3 +2,17 @@
 
 //specifically ans is the concatenation of two nums arrays
 // return the array of ans
+// Example 1:
+
+//Input: nums = [1,2,1]
+//Output: [1,2,1,1,2,1]
+
+function repeatArray(nums) {
+  let ans = new Array(nums.length * 2);
+
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = nums[i];
+    ans[i + nums.length] = nums[i];
+  }
+  return ans;
+}
