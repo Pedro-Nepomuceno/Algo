758dfffdf22451114876bd9715138f745cd30be2
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-09-17 19:17:43 -0700

    set problem

---
+++ Array/concatenationArray.js
@@ -0,0 +1,4 @@
+// giveen an integer array nums of length n you want to create an array ans of length 2n where ans[i] === nums[i] ans[i+n] == nums[i] for 0 <=i < n (o-indexed).
+
+//specifically ans is the concatenation of two nums arrays
+// return the array of ans
--- SlidingWindown.js
+++ SlidingWindown.js
@@ -23,3 +23,30 @@ function slidingWindow(nums) {
   }
   return [start, end];
 }
+// return the left and right index of the max subarray sum
+// assuming there is exaclty one result
+
+// [1,-4,1,2]
+
+function newSlidingWindow(array) {
+  let maxSum = array[0];
+  let currentSum = 0;
+  let left = 0;
+  let right = 0;
+  let tempLeft = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (currentSum === 0) {
+      tempLeft = i;
+    }
+    currentSum += array[i];
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+      left = tempLeft;
+      right = i;
+    }
+    if (currentSum < maxSum) {
+      currentSum = 0;
+    }
+  }
+  return [left, right];
+}
