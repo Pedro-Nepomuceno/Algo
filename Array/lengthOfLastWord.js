d5abe1f858bc4674205ea0c20415ccc555bdeefc
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-10-08 20:17:58 -0700

    possible solution

--- Array/lengthOfLastWord.js
+++ Array/lengthOfLastWord.js
@@ -7,3 +7,19 @@
 // Input: s = "   fly me   to   the moon  "
 // Output: 4
 // Explanation: The last word is "moon" with length 4.
+
+function lengthLastWord(s) {
+  let lastWord = [];
+  let count = 0;
+  for (let char = 0; i < s.length; i++) {
+    if (!char[i] == "") {
+      count + 1;
+      lastWord.push(char[i]);
+    } else if (char[i] == " " && char[i + 1] !== " ") {
+      lastword = [];
+      count = 0;
+      lastWord.push(char[i + 1]);
+    }
+    return lastWord.length;
+  }
+}
