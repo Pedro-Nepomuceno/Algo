a26832f8f137079401ad5a0e42d2d201c51725c3
Pedro-Nepomuceno <pedrohnlima@gmail.com>
2024-10-14 14:23:57 -0700

    linear solution

--- Array/lengthOfLastWord.js
+++ Array/lengthOfLastWord.js
@@ -12,12 +12,10 @@ function lengthLastWord(s) {
   let length = 0;
   let i = s.length - 1;
 
-  // Skip trailing spaces
   while (i >= 0 && s[i] === " ") {
     i--;
   }
 
-  // Count characters of the last word
   while (i >= 0 && s[i] !== " ") {
     length++;
     i--;
